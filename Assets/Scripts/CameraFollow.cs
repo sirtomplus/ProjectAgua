4d8bc83855feb9cbe667fa6b80d315aaa554b1b9
sirtomplus <tomphamhd@gmail.com>
2018-05-20 13:07:54 -0700

    Initial commit

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    GameObject PlayerObject;
+    const float DISTANCE_TO_FOLLOW = 0.5f; //In world points
+    const float INTERP_TIME = 0.1f;
+    const float MAX_POS = 2.9f;
+    const float MIN_POS = -2.9f;
+    const float DISTANCE_FROM_PLAYER = 1.75f;
+    void Awake()
+    {
+        PlayerObject = GameObject.FindGameObjectWithTag("Player");
+
+    }
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        var camPos = transform.position;
+        var playerPos = PlayerObject.transform.position;
+        if(Mathf.Abs(camPos.x - playerPos.x) >= DISTANCE_TO_FOLLOW)
+        {
+            var newCamPos = Vector3.Lerp(camPos, playerPos, INTERP_TIME);
+            if (newCamPos.x > MAX_POS)
+                newCamPos.x = MAX_POS;
+            else if (newCamPos.x < MIN_POS)
+                newCamPos.x = MIN_POS;
+            camPos.x = newCamPos.x;
+            
+        }
+        camPos.z = playerPos.z - DISTANCE_FROM_PLAYER;
+        transform.position = camPos;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class PlayerController : MonoBehaviour
+{
+    const float INTERP_TIME = 0.2f;
+    const float MAX_VELOCITY = 2f;
+
+    Rigidbody m_Rigidbody;
+    
+    float m_Distance = 0f;
+    float m_Speed = 0f;
+
+    bool m_IsAccelerating = false;
+    bool m_IsDragging;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(m_IsAccelerating)
+        {
+            m_Speed = Mathf.Lerp(m_Speed, MAX_VELOCITY, INTERP_TIME);
+            m_Rigidbody.velocity = transform.forward * m_Speed;
+        }
+        else
+        {
+            m_Speed = Mathf.Lerp(m_Speed, 0, INTERP_TIME);
+            m_Rigidbody.velocity = transform.forward * m_Speed;
+        }
+    }
+
+    void OnMouseDown()
+    {
+#if UNITY_ANDROID
+        var touch = Input.GetTouch(0);
+        var ray = Camera.main.ScreenPointToRay(touch.position);
+#else
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+#endif
+        m_Distance = Vector3.Distance(ray.origin, transform.position);
+        m_IsAccelerating = true;
+    }
+
+    void OnMouseDrag()
+    {
+#if UNITY_ANDROID
+        var touch = Input.GetTouch(0);
+        var ray = Camera.main.ScreenPointToRay(touch.position);
+#else
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+#endif
+        var pos = ray.origin + ray.direction * m_Distance;
+        pos.y = transform.position.y;
+        pos.z = transform.position.z;
+        transform.position = Vector3.Lerp(transform.position, pos, INTERP_TIME);
+    }
+
+    void OnMouseUp()
+    {
+        m_IsAccelerating = false;
+    }
+}
